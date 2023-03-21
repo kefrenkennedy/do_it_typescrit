143ec40c9f80296d5f9eb56e4ba0ed2eff6a1c4e
kefrenkennedy <kefrenkennedyav@gmail.com>
2023-03-21 08:36:56 -0300

    Refactor Input component to handle input variation based on focus and value, and add forwardRef to allow parent component to access input ref. Add form validation using yup and react-hook-form in Login page, and implement handleSignIn function to handle form submission

--- APP/src/components/Form/input.tsx
+++ APP/src/components/Form/input.tsx
@@ -10,31 +10,67 @@ import {
 
 import { FaExclamation } from "react-icons/fa";
 
-import { useState, useEffect, useCallback, useRef } from "react";
+import {
+  useState,
+  useEffect,
+  useCallback,
+  useRef,
+  ForwardRefRenderFunction,
+  forwardRef,
+} from "react";
 
-import { FieldError } from "react-hook-form";
 import { IconType } from "react-icons/lib";
+import { FieldError, FieldErrors } from "react-hook-form";
 
 interface InputProps extends ChakraInputProps {
   name: string;
   label?: string;
-  error?: FieldError | null;
+  error?: any;
   icon?: IconType;
 }
 
-export const Input = ({
-  name,
-  error = null,
-  icon: Icon,
-  label,
-  ...rest
-}: InputProps) => {
+type inputVariationOptions = {
+  [key: string]: string;
+};
+
+const inputVariation: inputVariationOptions = {
+  error: "red.500",
+  default: "gray.200",
+  focus: "purple.800",
+  filled: "green.600",
+};
+
+const InputBase: ForwardRefRenderFunction<HTMLInputElement, InputProps> = (
+  { name, error = null, icon: Icon, label, ...rest },
+  ref
+) => {
+  const [value, setValue] = useState("");
+  const [variation, setVariation] = useState("default");
+
+  useEffect(() => {
+    if (error) {
+      return setVariation("error");
+    }
+  }, [error]);
+
+  const handleInputFocus = useCallback(() => {
+    if (!error) {
+      setVariation("focus");
+    }
+  }, [error]);
+
+  const handleInputBlur = useCallback(() => {
+    if (value.length > 1 && !error) {
+      return setVariation("filled");
+    }
+  }, [error, value]);
+
   return (
-    <FormControl>
+    <FormControl isInvalid={!!error}>
       {!!label && <FormLabel>{label}</FormLabel>}
       <InputGroup flexDirection="column">
         {Icon && (
-          <InputLeftElement mt="2.5">
+          <InputLeftElement color={inputVariation[variation]} mt="2.5">
             <Icon />
           </InputLeftElement>
         )}
@@ -42,11 +78,17 @@ export const Input = ({
         <ChakraInput
           name={name}
           bg="gray.50"
+          color={inputVariation[variation]}
+          onChangeCaptureValue={(e: any) => setValue(e.currentTarget.value)}
+          borderColor={inputVariation[variation]}
+          onFocus={handleInputFocus}
+          onBlurCapture={handleInputBlur}
           variant="outline"
           _hover={{ bgColor: "gray.100" }}
           _placeholder={{ color: "gray.300" }}
           size="lg"
           h="60px"
+          ref={ref}
           {...rest}
         />
 
@@ -55,3 +97,5 @@ export const Input = ({
     </FormControl>
   );
 };
+
+export const Input = forwardRef(InputBase);
--- APP/src/pages/Login/index.tsx
+++ APP/src/pages/Login/index.tsx
@@ -1,15 +1,39 @@
-import { Heading, Flex, Text, Grid, Image, VStack } from "@chakra-ui/react";
-import { useForm } from "react-hook-form";
+import {
+  Heading,
+  Flex,
+  Text,
+  Grid,
+  Image,
+  VStack,
+  Button,
+} from "@chakra-ui/react";
+import { SubmitHandler, useForm } from "react-hook-form";
 import { FaEnvelope, FaLock } from "react-icons/fa";
 import LogoSecondary from "../../assets/logo-secondary.png";
 import { Input } from "../../components/Form/input.jsx";
+import * as yup from "yup";
+import { yupResolver } from "@hookform/resolvers/yup";
+
+const signInSchema = yup.object().shape({
+  email: yup.string().required("Email obrigatório").email("Email inválido"),
+  password: yup.string().required("Senha obrigatória"),
+});
+
+interface SignInData {
+  email: string;
+  password: string;
+}
 
 export const Login = () => {
   const {
     formState: { errors },
     register,
     handleSubmit,
-  } = useForm();
+  } = useForm<SignInData>({
+    resolver: yupResolver(signInSchema),
+  });
+
+  const handleSignIn: SubmitHandler<SignInData> = (data) => console.log(data);
 
   return (
     <Flex
@@ -34,9 +58,10 @@ export const Login = () => {
           </Text>
         </Grid>
         <Grid
+          onSubmit={handleSubmit(handleSignIn)}
           as="form"
           mt="4"
-          w="100%"
+          w="50%"
           padding="30px 15px"
           border="3px solid"
           borderColor="gray.100"
@@ -45,9 +70,23 @@ export const Login = () => {
         >
           <Heading size="lg">Bem vindo de volta!</Heading>
           <VStack mt="6" spacing="5">
-            <Input placeholder="Digite seu login" icon={FaEnvelope} name="email" />
-            <Input placeholder="Digite sua senha" icon={FaLock} {...register("password")} />
+            <Input
+              placeholder="Digite seu login"
+              icon={FaEnvelope}
+              label="Login"
+              type="email"
+              error={errors.email}
+              {...register("email")}
+            />
+            <Input
+              placeholder="Digite sua senha"
+              icon={FaLock}
+              error={errors.password}
+              type="password"
+              {...register("password")}
+            />
           </VStack>
+          <Button type="submit">Entrar</Button>
         </Grid>
       </Flex>
     </Flex>
