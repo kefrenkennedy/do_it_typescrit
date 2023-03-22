a54db71550f4af1e566eb0ae80bdb6a1d4c55735
kefrenkennedy <kefrenkennedyav@gmail.com>
2023-03-22 11:00:22 -0300

    Add user name and logout button to Header menu drawer in Chakra UI

--- APP/src/components/Header/Menu.tsx
+++ APP/src/components/Header/Menu.tsx
@@ -1,11 +1,18 @@
 import {
+  Box,
+  Center,
   Drawer,
   DrawerBody,
   DrawerContent,
   DrawerHeader,
   DrawerOverlay,
+  Flex,
+  Heading,
+  Text,
+  theme,
 } from "@chakra-ui/react";
 import { useAuth } from "../../contexts/AuthContext";
+import { FiLogOut } from "react-icons/fi";
 
 interface MenuProps {
   isOpen: boolean;
@@ -17,14 +24,40 @@ export const Menu = ({ isOpen, onClose }: MenuProps) => {
 
   return (
     <>
-      <Drawer placement="top" onClose={onClose} isOpen={isOpen}>
-        <DrawerOverlay />
-        <DrawerContent>
-          <DrawerHeader borderBottomWidth="1px">Basic Drawer</DrawerHeader>
+      <Drawer isOpen={isOpen} placement="top" onClose={onClose}>
+        <DrawerOverlay mt="8vh" />
+        <DrawerContent ml="auto" mt="80px" w={["450px", "350px"]}>
+          <DrawerHeader
+            borderBottomWidth="1px"
+            borderColor="gray.50"
+            color="gray.400"
+          >
+            {user.name}
+          </DrawerHeader>
           <DrawerBody>
-            <p>Some contents...</p>
-            <p>Some contents...</p>
-            <p>Some contents...</p>
+            <Flex
+              align="center"
+              onClick={signOut}
+              _hover={{ cursor: "pointer" }}
+            >
+              <Center
+                w="60px"
+                h="60px"
+                bg="red.600"
+                fontSize="2xl"
+                borderRadius="md"
+              >
+                <FiLogOut color={theme.colors.white} />
+              </Center>
+              <Box ml="4">
+                <Heading as="h2" fontSize="lg">
+                  Sair da minha conta
+                </Heading>
+                <Text color="gray.300" fontSize="small">
+                  Sair da minha conta agora
+                </Text>
+              </Box>
+            </Flex>
           </DrawerBody>
         </DrawerContent>
       </Drawer>
