1671ad10b4978cb241223eb20f03033a34efa6fa
kefrenkennedy <kefrenkennedyav@gmail.com>
2023-03-22 11:25:48 -0300

    Add Card component and SearchBox component to Dashboard page in a grid layout

--- APP/src/pages/Dashboard/index.tsx
+++ APP/src/pages/Dashboard/index.tsx
@@ -1,13 +1,28 @@
-import { Box, Button, Text } from "@chakra-ui/react";
+import { Box, Button, Grid, GridItem, Text } from "@chakra-ui/react";
+import { Card } from "../../components/Card";
+import { SearchBox } from "../../components/Form/SearchBox";
 import { Header } from "../../components/Header";
 import { useAuth } from "../../contexts/AuthContext";
 
 export const Dashboard = () => {
   const { signOut } = useAuth();
+
   return (
     <>
       <Box>
         <Header />
+        <Grid
+          w="100%"
+          templateColumns="repeat(auto-fill, minmax(420px, 1fr))"
+          gap={10}
+          paddingX="8"
+          mt="8"
+        >
+          {[1, 2, 3, 4, 5, 6].map((_) => (
+            <Card />
+          ))}
+        </Grid>
+        <SearchBox />
       </Box>
     </>
   );
