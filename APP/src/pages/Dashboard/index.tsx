3f3e56cacf990464e31620df126ec3bbd9a7eeda
kefrenkennedy <kefrenkennedyav@gmail.com>
2023-03-30 16:42:22 -0300

    Add modal for creating tasks and display a message to create a task if there are no tasks present

--- APP/src/pages/Dashboard/index.tsx
+++ APP/src/pages/Dashboard/index.tsx
@@ -18,6 +18,8 @@ import { useTasks } from "../../contexts/TasksContext";
 import { useState, useEffect } from "react";
 import { ModalTaskDetail } from "../../components/Modal/ModalTaskDetail";
 import { CardSkeleton } from "../../components/Skeleton/CardSkeleton";
+import { ModalCreateTask } from "../../components/Modal/ModalCreateTask";
+import { FaClipboard } from "react-icons/fa";
 
 interface Task {
   id: string;
@@ -37,6 +39,12 @@ export const Dashboard = () => {
     isOpen: isTaskDetailOpen,
     onOpen: onTaskDetailOpen,
     onClose: onTaskDetailClose,
+  } = useDisclosure();
+
+  const {
+    isOpen: isCreateTaskOpen,
+    onOpen: onCreateTaskOpen,
+    onClose: onCreateTaskClose,
   } = useDisclosure();
 
   useEffect(() => {
@@ -115,24 +123,70 @@ export const Dashboard = () => {
         onClose={onTaskDetailClose}
         task={selectedTask}
       />
-      <Box>
-        <Header />
-        <SearchBox />
-        <Grid
-          w="100%"
-          templateColumns="repeat(auto-fill, minmax(420px, 1fr))"
-          gap={10}
-          paddingX="8"
-          mt="8"
-        >
-          {loading ? (
-            <CardSkeleton repeatCount={9} />
-          ) : (
-            tasks.map((task) => <Card task={task} onClick={handleClick} />)
-          )}
-        </Grid>
-        <SearchBox />
-      </Box>
+      {!loading && !tasks.length ? (
+        <>
+          <ModalCreateTask
+            isOpen={isCreateTaskOpen}
+            onClose={onCreateTaskClose}
+          />
+          <Header />
+          <Box
+            mt="4"
+            w="90vw"
+            paddingY="16"
+            paddingX={["6", "0"]}
+            ml="5vw"
+            justifyContent="center"
+            textAlign="center"
+            borderWidth="2px"
+            borderColor="gray.200"
+            borderStyle="dashed"
+          >
+            <Center fontSize="5xl">
+              <FaClipboard color="#bdbdbd" />
+            </Center>
+            <Heading fontSize="4xl" as="h1" mt="4">
+              Vamos criar sua primeira tarefa
+            </Heading>
+            <Text color="gray.400" mt="6">
+              Insira sua meta e mostre a você mesmo <br /> sua capacidade em
+              cumprir
+              <Text fontWeight="bold" color="gray.900">
+                suas atividades
+              </Text>
+            </Text>
+            <Button
+              padding="6"
+              mt="6"
+              bgColor="purple.800"
+              color="white"
+              _hover={{ bg: "purple.900" }}
+              onClick={onCreateTaskOpen}
+            >
+              Criar sua primeira tarefa
+            </Button>
+          </Box>
+        </>
+      ) : (
+        <Box>
+          <Header />
+          <SearchBox />
+          <Grid
+            w="100%"
+            templateColumns="repeat(auto-fill, minmax(420px, 1fr))"
+            gap={10}
+            paddingX="8"
+            mt="8"
+          >
+            {loading ? (
+              <CardSkeleton repeatCount={9} />
+            ) : (
+              tasks.map((task) => <Card task={task} onClick={handleClick} />)
+            )}
+          </Grid>
+          <SearchBox />
+        </Box>
+      )}
     </>
   );
 };
