ecd04c6a7df5728d969f597e4058e665d4e488ea
kefrenkennedy <kefrenkennedyav@gmail.com>
2023-04-14 05:31:31 -0300

    13th

--- Backend/src/app.ts
+++ Backend/src/app.ts
@@ -1,14 +1,17 @@
 import 'express-async-errors';
 import express from 'express';
-
 import cors from 'cors';
-import errorMiddleware from "./middlewares/errorMiddleware"
+import errorMiddleware from './middlewares/errorMiddleware';
 import userRoutes from './routes/userRoutes';
 import authRotes from './routes/authRoutes';
 import taskRoutes from './routes/taskRoutes';
+import swaggerUi from 'swagger-ui-express';
+import fs from 'fs/promises';
+import YAML from 'yaml';
 
 class App {
   server: any;
+
   constructor() {
     this.server = express();
     this.middlewares();
@@ -20,16 +23,20 @@ class App {
       methods: 'GET,POST,PATCH,DELETE',
       origin: '*',
     };
-
     this.server.use(cors(options));
   }
 
   async middlewares() {
-    this.enableCors();
+    await this.enableCors();
     this.server.use(express.json());
   }
 
   async routes() {
+    const filePath = './Do_It.postman_collection.json-SwaggerYaml.yaml';
+    const fileContents = await fs.readFile(filePath, 'utf8');
+    const swaggerDocument = YAML.parse(fileContents);
+
+    this.server.use('/', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
     this.server.use('/login', authRotes);
     this.server.use('/dashboard/user', userRoutes);
     this.server.use('/dashboard/task', taskRoutes);
